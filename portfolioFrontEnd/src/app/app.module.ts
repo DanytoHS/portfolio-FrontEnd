87db4692f1ad1663e3c55b5c4c97a0fa81a5cc37
Danilo Bascourleguy <danilobasco4@hotmail.com>
2023-05-13 10:54:19 -0300

    Finalizado de Footer y arreglos menores

--- portfolioFrontEnd/src/app/app.module.ts
+++ portfolioFrontEnd/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardandsoftComponent } from './components/hardandsoft/hardandsoft.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,12 +27,13 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HardandsoftComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
